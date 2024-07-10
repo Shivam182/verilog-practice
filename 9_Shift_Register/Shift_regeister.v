ccbc9e3412c65c7f8128bfdc8e1b68414d9ae9ee
Shivam Yadav <yadavshivam24012002@gmail.com>
2024-07-10 12:03:01 +0530

    import statement

--- 9_Shift_Register/Shift_regeister.v
+++ 9_Shift_Register/Shift_regeister.v
@@ -6,8 +6,14 @@ module shift_reg(
 	output k,
 	input rstn);
 
-	
+	wire [2:0] q_net;	
 
+	// 3-bit shift register
+	
+	d_ff d1 (.D(D) , .clk(clk), .rstn(rstn), .Q(q_net[0]));
+	d_ff d2 (.D(q_net[0]), .clk(clk), .rstn(rstn), .Q(q_net[1]));
+	d_ff d3 (.D(q_net[1]), .clk(clk), .rstn(rstn), .Q(q_net[2]));
+	
 
 
 
